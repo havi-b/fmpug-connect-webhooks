d0cf8c8a67b75416330ce835748ac8e791cecb91
Hafzul Bhuiyan <havi@Hafzuls-MBP.home>
2020-03-06 02:45:56 -0600

    Changed REGEX to search for '!lyrics' in string && Added /test GET route

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,20 @@ const router = express.Router();
 
 const axios = require('axios');
 
-router.get('/lyrics', async (req, res) => {
+router.post('/lyrics', async (req, res) => {
   let string = req.body.string;
 
-  let result = string.match(/([A-Z]|\s)*\s(-)\s([A-Z]|\s)*/i);
-  if (!result) return res.status(400).json({ error: 'Invalid input!' });
-
+  let result = req.body.string.match(
+    /\!lyrics\s([A-Z]|\s)*\s(-)\s([A-Z]|\s)*/i
+  );
+  if (!result)
+    return res.status(400).json({
+      status: 400,
+      message: 'Invalid input!',
+      search: string,
+      success: false
+    });
+  string = string.split('!lyrics ')[1];
   let array = string.split(' - ');
   let song = array[0];
   let artist = array[1];
@@ -16,11 +24,20 @@ router.get('/lyrics', async (req, res) => {
   axios
     .get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
     .then(response => {
-      res.send(response.data.lyrics);
+      res.send(response.data);
     })
     .catch(err =>
-      res.status(400).json({ error: 'Sorry... Lyrics not available...' })
+      res.status(400).json({
+        status: 400,
+        message: 'Sorry... Lyrics not available...',
+        search: string,
+        success: false
+      })
     );
+});
+
+router.get('/test', (req, res) => {
+  res.json({ data: 'All is well!', success: true });
 });
 
 module.exports = router;
